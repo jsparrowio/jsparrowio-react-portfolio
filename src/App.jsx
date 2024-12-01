d9a9c09c884e38d78dbcaf0fba8385e8cc54aaab
jsparrowio <jsparrowio@outlook.com>
2024-12-01 15:51:50 -0800

    added starter files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import './App.css';
+//import  from '';
+
+function App() {
+  return (
+    <div className="portfolio-app">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
