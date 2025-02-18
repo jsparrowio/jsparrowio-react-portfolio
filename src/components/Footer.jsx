6726df22e32adfca1d6ab1c6dbbefc7eafe3acb9
jsparrowio <jsparrowio@outlook.com>
2025-02-17 16:51:43 -0800

    added footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer>
       <section>
-        <a href=""><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/medium/light/linkedin.svg"></img> </a>
-        <a href=""><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/medium/light/github.svg"></img></a><br />
+        <a href="https://www.linkedin.com/in/joshua-garrett-121790297/"><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/medium/light/linkedin.svg"></img> </a>
+        <a href="https://github.com/jsparrowio"><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/medium/light/github.svg"></img></a><br />
         <b>&copy; 2025 jsparrowio Website Design</b>
       </section>
     </footer>
