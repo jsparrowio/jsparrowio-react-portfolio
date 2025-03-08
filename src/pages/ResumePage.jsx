c0f4ab2072ed87ce291920d84c8a1ce837fadb3c
jsparrowio <jsparrowio@outlook.com>
2025-03-07 18:24:52 -0800

    added new resume link

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -7,7 +7,7 @@ export default function ResumePage() {
       <h2>Resume</h2>
       <section className="page-section" id="aboutme">
         <div id="placeholder">
-          <a href="https://docs.google.com/document/d/16_u4JoRZl38GEvdG8X8C-rB6o40_h6Nz/edit?usp=sharing&ouid=113933529286083268853&rtpof=true&sd=true"><img src="https://www.svgrepo.com/show/5908/text-document.svg" className="pdf" style={{ maxHeight: '15px', maxWidth: '15px' }}></img>Download My Resume!</a>
+          <a href="https://docs.google.com/document/d/1rj9FzBkYMO6F1fcSwzQUrxo-EytDBoBK/edit?usp=sharing&ouid=113933529286083268853&rtpof=true&sd=true"><img src="https://www.svgrepo.com/show/5908/text-document.svg" className="pdf" style={{ maxHeight: '15px', maxWidth: '15px' }}></img>Download My Resume!</a>
         </div>
         <div className="section-content-div" id="resume">
           <h3>Front End Profeciencies</h3>
